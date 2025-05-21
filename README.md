5f5450321cc839520cb6d482b6aff2e4d60a3ebe
pratchayapol <pratchayapol2543@gmail.com>
2025-05-21 16:21:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+1.git clone https://github.com/pratchayapol/ecp_online_petition_php.git
+
+
+2.install docker 
+
+
+3.cd เข้าไปที่ path งาน
+
+
+4.docker compose up -d
+
+สำเร็จ
+(อย่าลืม Set timezone utc+7 ด้วยนะครับ)
+ 
+ฐานข้อมูลใน [http://203.158.201.73:8080](http://203.158.201.73:8080/)
---
+++ src/connect/dbcon.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "mariadb";
+$port = 3306;
+$username = "root";
+$password = "adminecp";
+$dbname = "ecp_req";
+
+try {
+    $dsn = "mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // ตั้งค่า PDO ให้แสดงข้อผิดพลาดแบบ exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
+} catch (PDOException $e) {
+    echo "เชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage();
+}
+?>
