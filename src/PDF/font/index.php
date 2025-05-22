a9f1ce5ccdd328e0a1827525b67d1fb8fe3f5d07
pratchayapol <pratchayapol2543@gmail.com>
2025-05-22 11:18:13 +0700

    okay

--- src/PDF/font/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('../makefont/makefont.php');
-MakeFont('THSarabun.ttf','cp874');
-MakeFont('THSarabun Italic.ttf','cp874');
-MakeFont('THSarabun Bold.ttf','cp874');
-MakeFont('THSarabun Bold Italic.ttf','cp874');
-?>
