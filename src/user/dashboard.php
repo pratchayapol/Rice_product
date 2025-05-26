b4b28d28b5374817fc596c6f8543a4a9d26789a1
pratchayapol <pratchayapol2543@gmail.com>
2025-05-26 13:57:31 +0700

    ccx

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -24,7 +24,7 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
-<body class="flex items-center justify-center min-h-screen bg t1">
+<body class="flex items-center justify-center min-h-screen bg t1 pt-20">
     <?php include '../loadtab/h.php'; ?>
     <!-- Navigation Bar -->
     <?php include './plugin/navbar.php' ?>
