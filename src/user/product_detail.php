635419f64c827cc7e1d4dc1c0a96ad03850c608c
pratchayapol <pratchayapol2543@gmail.com>
2025-05-27 11:17:03 +0700

    แป

---
+++ src/user/product_detail.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="th">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>รายละเอียดผลิตภัณท์</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Custom fonts for this template-->
+    <link rel="shortcut icon" href="../image/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/bg1.css">
+    <!-- animation -->
+    <link rel="stylesheet" href="../css/animation.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body class="bg t1">
+    <?php include '../loadtab/h.php'; ?>
+    <!-- Navigation Bar -->
+    <?php include './plugin/navbar.php'; ?>
+
+
+    
+    <script>
+        document.getElementById("menu-toggle").addEventListener("click", function() {
+            const menu = document.getElementById("mobile-menu");
+            menu.classList.toggle("hidden");
+        });
+    </script>
+    <?php include '../loadtab/f.php'; ?>
+</body>
+
+</html>
