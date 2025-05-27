fcbb78dc43fb3e7909f94e72457ec08ce6ad986b
pratchayapol <pratchayapol2543@gmail.com>
2025-05-27 14:56:41 +0700

    cx

--- src/user/profile.php
+++ src/user/profile.php
@@ -47,7 +47,7 @@ include '../connect/dbcon.php';
                 <h2 class="text-xl font-semibold text-gray-800 mb-2"><?= htmlspecialchars($_SESSION['user']['name']) ?></h2>
                 <p class="text-gray-600 text-sm md:text-base"><?= htmlspecialchars($_SESSION['user']['email']) ?></p>
                 <br>
-                <a href="./google_auth?logout=true"
+                <a href="../google_auth?logout=true"
                     class="inline-block bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-6 rounded-lg transition duration-200">
                     ออกจากระบบ
                 </a>
