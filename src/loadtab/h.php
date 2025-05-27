efa329a6087ac6b1a687fd9639c6d6ed8a9c5a92
pratchayapol <pratchayapol2543@gmail.com>
2025-05-27 15:00:28 +0700

    แแป

--- src/loadtab/h.php
+++ src/loadtab/h.php
@@ -13,16 +13,24 @@
     width: 100%;
     height: 4px;
     background-color: #ccc;
-    /* สีพื้นหลังเริ่มต้น */
     z-index: 9999;
   }
 
   #progress-bar {
     width: 0%;
     height: 100%;
-    background-color: #f00;
-    /* สีที่ต้องการเปลี่ยน */
+    background: linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet);
+    background-size: 400% 100%;
+    animation: rainbow 3s linear infinite;
     transition: width 0.3s ease-out;
   }
-  .t1 {font-family: 'Noto Sans Thai', sans-serif;}
+
+  @keyframes rainbow {
+    0% { background-position: 0% 50%; }
+    100% { background-position: 100% 50%; }
+  }
+
+  .t1 {
+    font-family: 'Noto Sans Thai', sans-serif;
+  }
 </style>
