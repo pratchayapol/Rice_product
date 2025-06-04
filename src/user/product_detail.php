cf524d5546b1c7d923c65d9b159f34da8f8baa18
pratchayapol <pratchayapol2543@gmail.com>
2025-06-04 09:42:36 +0700

    cx

--- src/user/product_detail.php
+++ src/user/product_detail.php
@@ -238,7 +238,7 @@ if ($id > 0) {
                                     <div class="bg-white p-4 rounded-lg">
                                         <h4 class="inline-block font-bold mb-4 bg-white px-6 py-2 rounded-full text-sm mx-auto shadow"> ข้อมูลโภชนาการ </h4>
                                         <div class="overflow-x-auto">
-                                            <canvas id="nutritionChart" class="w-full" style="min-height: 1200px;"></canvas>
+                                            <canvas id="nutritionChart" class="w-full t1" style="min-height: 1200px;"></canvas>
                                         </div>
                                     </div>
                                 </div>
