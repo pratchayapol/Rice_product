4c256fc2b0cd2860902abfb0d546a5d852f0d079
pratchayapol <pratchayapol2543@gmail.com>
2025-06-06 16:19:31 +0700

    cx

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -190,7 +190,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
 
                 <!-- Pie Chart -->
                 <div class="flex-1 bg-white rounded-xl shadow p-3">
-                    <canvas id="productChart" style="height:175px;"></canvas>
+                    <canvas id="productChart" style="height:250px;"></canvas>
                 </div>
             </div>
         </div>
