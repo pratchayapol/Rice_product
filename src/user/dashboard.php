96840622b42f5866c4bf9bdf78b7274c6a602dd8
pratchayapol <pratchayapol2543@gmail.com>
2025-06-07 17:03:46 +0700

    gf

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -232,7 +232,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
                 responsive: true,
                 maintainAspectRatio: false,
                 layout: {
-                    padding: 25 // 👈 ลดขนาดวงกลมโดยเพิ่ม padding รอบขอบ
+                    padding: 35 // 👈 ลดขนาดวงกลมโดยเพิ่ม padding รอบขอบ
                 },
                 plugins: {
                     legend: {
