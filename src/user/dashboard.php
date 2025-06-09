08b338016f8c553d76e683debcafd817ba97115a
pratchayapol <pratchayapol2543@gmail.com>
2025-06-09 09:12:36 +0700

    อแ

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -144,7 +144,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                         </span>
-                        <ul id="suggestions" class="absolute z-10 w-full bg-white border border-gray-300 rounded-b-lg mt-1 max-h-60 overflow-y-auto hidden text-left"></ul>
+                        <ul id="suggestions" class="absolute z-10 w-full bg-white border border-gray-300 rounded-b-lg mt-1 max-h-60 overflow-y-auto hidden text-left scrollbar-hide"></ul>
                     </div>
                 </div>
 
