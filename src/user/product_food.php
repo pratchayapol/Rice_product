e65e7f43659feef345ca8cafd4d7a923aeb28a18
pratchayapol <pratchayapol2543@gmail.com>
2025-06-09 10:54:57 +0700

    xz

--- src/user/product_food.php
+++ src/user/product_food.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php'; // ปรับ path ให้ตรงกับที่ติดตั้ง vendor
 
 use JasonGrimes\Paginator;
 
