84edd9430e6a73f6e900990f6cc4247ca9e1370a
pratchayapol <pratchayapol2543@gmail.com>
2025-06-09 11:20:21 +0700

    อแ

--- src/user/product_food.php
+++ src/user/product_food.php
@@ -102,10 +102,10 @@ $products = $stmt->fetchAll();
                                         class="rounded-xl mb-4 w-full h-40 object-cover" />
 
                                     <div class="flex flex-col gap-2 w-full">
-                                        <div class="w-full px-4 py-1 rounded-full text-sm text-gray-700 shadow hover:shadow-md hover:bg-gray-100 transition text-center">
+                                        <div class="w-full px-4 py-1 rounded-full text-sm text-gray-700 shadow bg-sky-100 hover:bg-gray-200 transition text-center">
                                             <?= htmlspecialchars($product['product_name']) ?>
                                         </div>
-                                        <div class="w-full px-4 py-1 rounded-full text-sm text-gray-700 shadow hover:shadow-md hover:bg-gray-100 transition text-center">
+                                        <div class="w-full px-4 py-1 rounded-full text-sm text-gray-700 shadow bg-sky-100 hover:bg-gray-200 transition text-center">
                                             <?= htmlspecialchars($product['rice_variety_th_name']) ?>
                                         </div>
                                     </div>
