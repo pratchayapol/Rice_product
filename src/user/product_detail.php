b5f89028eb65231809a27cd976bd917b693303fa
pratchayapol <pratchayapol2543@gmail.com>
2025-06-11 13:29:19 +0700

    แป

--- src/user/product_detail.php
+++ src/user/product_detail.php
@@ -168,14 +168,14 @@ if ($id > 0) {
                                     <div class="bg-white p-4 rounded-lg mb-4">
                                         <h4 class="inline-block font-bold mb-2 bg-white px-4 py-2 rounded-full text-sm w-fit px-4 py-1 mx-auto shadow">อุปกรณ์</h4>
                                         <div style='border:0px solid #ccc; padding:10px;'>
-                                            <?php echo $cooking_equipment; ?>
+                                            <?php echo $ingredients_and_equipment; ?>
                                         </div>
 
                                     </div>
                                     <div class="bg-white p-4 rounded-lg">
                                         <h4 class="inline-block font-bold mb-2 bg-white px-4 py-2 rounded-full text-sm w-fit px-4 py-1 mx-auto shadow">วิธีทำ</h4>
                                         <div style='border:0px solid #ccc; padding:10px;'>
-                                            <?php echo $recipe; ?>
+                                            <?php echo $instructions; ?>
                                         </div>
                                     </div>
                                 </div>
