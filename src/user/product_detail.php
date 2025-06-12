ef128f6e37b4002e365df04094a8bad1ba54c3f6
pratchayapol <pratchayapol2543@gmail.com>
2025-06-12 10:58:48 +0700

    cx

--- src/user/product_detail.php
+++ src/user/product_detail.php
@@ -57,7 +57,7 @@ if ($id > 0) {
                 <div class="grid grid-cols-12 gap-1">
 
                     <!-- Sidebar -->
-                    <div class="col-span-12 md:col-span-4 rounded-lg p-6 text-center text-gray-800 flex flex-col items-center" style="background-color: #8b8550;">
+                    <div class="col-span-12 md:col-span-3 rounded-lg p-6 text-center text-gray-800 flex flex-col items-center" style="background-color: #8b8550;">
                         <h3 class="text-xl font-bold text-center text-gray-800 mb-4 bg-white px-4 py-2 rounded-full">ผลิตภัณฑ์ <?php echo $product_name ?></h3>
 
                         <?php
@@ -80,7 +80,7 @@ if ($id > 0) {
                     </div>
 
                     <!-- Content -->
-                    <div class="col-span-12 md:col-span-8 bg-blue-50 rounded-lg p-6">
+                    <div class="col-span-12 md:col-span-9 bg-blue-50 rounded-lg p-6">
                         <h3 class="text-xl font-bold text-center text-gray-800 mb-4 bg-white px-4 py-2 rounded-full">
                             จาก ข้าวพันธุ์<?php echo $thai_name . ' ( ' . $english_name . ' )' ?>
                         </h3>
