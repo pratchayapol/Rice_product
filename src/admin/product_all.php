59191d8f84bc93ca322ea4182d488d621cbd21bf
pratchayapol <pratchayapol2543@gmail.com>
2025-06-17 14:00:36 +0700

    แป

--- src/admin/product_all.php
+++ src/admin/product_all.php
@@ -107,6 +107,10 @@ $products_medical = $stmt->fetchAll();
                                 <h3 class="text-xl font-bold text-center text-gray-800 mb-4 bg-rose-300 px-4 py-2 rounded-full shadow-md">
                                     ผลิตภัณฑ์อาหาร
                                 </h3>
+                                <a href="add_product_food.php"
+                                    class="bg-green-500 hover:bg-green-600 text-white text-sm font-medium py-2 px-4 rounded-full shadow">
+                                    เพิ่มผลิตภัณฑ์
+                                </a>
                                 <div class="overflow-x-auto p-6">
                                     <table id="productTable1" class="min-w-full table-auto border-collapse border border-gray-300 text-sm text-left">
                                         <thead class="bg-rose-200 text-gray-800">
