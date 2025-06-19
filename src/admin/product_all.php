5a78aeba79d34f3772b2430c35685bf0d426e100
pratchayapol <pratchayapol2543@gmail.com>
2025-06-19 16:11:54 +0700

    แป

--- src/admin/product_all.php
+++ src/admin/product_all.php
@@ -182,7 +182,7 @@ $products_medical = $stmt->fetchAll();
                                     ผลิตภัณฑ์เวชสำอาง
                                 </h3>
                                 <div class="flex justify-end mb-4">
-                                    <a href="add_product?type=cosmetic"
+                                    <a href="add_product_cosmetic"
                                         class="bg-green-500 hover:bg-green-600 text-white text-sm font-medium py-2 px-4 rounded-full shadow">
                                         เพิ่มผลิตภัณฑ์
                                     </a>
