2b01c8b1240bdc9d2c27fc7fef7cec2d1ab4b178
pratchayapol <pratchayapol2543@gmail.com>
2025-06-19 22:24:25 +0700

    cx

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -203,7 +203,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
     <script>
         Chart.defaults.font.family = 'Noto Sans Thai';
 
-        function animateCounter(element, start, end, duration = 1000) {
+        function animateCounter(element, start, end, duration = 500) {
             if (start === end) {
                 element.textContent = end;
                 return;
