37164e455a1aac8615601c766190dcd607e3e46c
pratchayapol <pratchayapol2543@gmail.com>
2025-06-20 14:39:32 +0700

    แป

--- src/swagger.php
+++ src/swagger.php
@@ -1,10 +1,16 @@
 <!-- index.html -->
 <!DOCTYPE html>
 <html>
+
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Custom fonts for this template-->
+  <link rel="shortcut icon" href="../image/favicon.ico" type="image/x-icon">
   <title>Swagger UI</title>
   <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
 </head>
+
 <body>
   <div id="swagger-ui"></div>
   <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
@@ -15,4 +21,5 @@
     });
   </script>
 </body>
+
 </html>
