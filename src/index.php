68c71922a014338f7a311909a4af86122f639bc7
pratchayapol <pratchayapol@github.com>
2025-07-01 23:20:49 +0700

    กห

--- src/index.php
+++ src/index.php
@@ -77,7 +77,12 @@ $line_login_url = 'https://liff.line.me/2007460484-WlA3R3By';
                     alt="LINE Logo" class="w-6 h-6 bg-white rounded p-1">
                 Sign in with LINE
             </a>
-
+            <a
+                href="https://riceproduct.pcnone.com/%E0%B8%84%E0%B8%B9%E0%B9%88%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%8A%E0%B9%89%E0%B8%87%E0%B8%B2%E0%B8%99%20%E0%B9%80%E0%B8%A7%E0%B9%87%E0%B8%9A%E0%B9%81%E0%B8%AD%E0%B8%9B%E0%B8%9E%E0%B8%A5%E0%B8%B4%E0%B9%80%E0%B8%84%E0%B8%8A%E0%B8%B1%E0%B8%99%20%E0%B8%90%E0%B8%B2%E0%B8%99%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B9%81%E0%B8%9B%E0%B8%A3%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95%E0%B8%A0%E0%B8%B1%E0%B8%93%E0%B8%91%E0%B9%8C%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%A7.pdf"
+                target="_blank"
+                class="inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition">
+                ดาวน์โหลดคู่มือการใช้งาน
+            </a>
         </div>
     </div>
     <?php include './loadtab/f.php'; ?>
