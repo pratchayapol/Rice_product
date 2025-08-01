cd42cd23624760ac5e54f7b17cd8c7c0cdf8497c
pratchayapol <pratchayapol@github.com>
2025-08-01 15:20:17 +0700

    แป

--- src/user/dashboard.php
+++ src/user/dashboard.php
@@ -393,7 +393,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
             chatInput.value = '';
             chatSendBtn.disabled = true;
             try {
-                const response = await fetch('https://rice_product_chat.pcnone.com/api', {
+                const response = await fetch('https://rice_product_chat.pcnone.com/api/chat', {
                     method: 'POST',
                     headers: {
                         accept: 'application/json',
